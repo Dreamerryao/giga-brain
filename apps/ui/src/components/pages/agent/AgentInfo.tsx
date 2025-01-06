b23bc7ee6c61c86533cebf882d8b7b571eb19ada
Mitchel Kelonye <kelonyemitchel@gmail.com>
2025-01-06 22:29:28 +0400

    feat: agent header card fixes

--- apps/ui/src/components/pages/agent/AgentInfo.tsx
+++ apps/ui/src/components/pages/agent/AgentInfo.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import { Clock, Coins } from 'lucide-react';
+import { PropsWithChildren } from 'react';
+import * as anchor from '@coral-xyz/anchor';
+import { Clock, Coins, LucideIcon } from 'lucide-react';
 
 import { PuzzleRemainingTime } from '@/components/shared/PuzzleRemainingTime';
 import { FormatCurrency } from '@/components/shared/FormatCurrency';
@@ -13,36 +15,75 @@ export function AgentInfo() {
 
   return (
     <>
-      <div
-        className={cn(
-          'flex items-center space-x-2 px-4 h-full rounded-xl border border-zinc-700/50 leading-none',
-          {
-            'bg-amber-600/10 border-amber-600/20': isInFinalTimer,
-          }
-        )}
+      <Card
+        className={cn(' border-zinc-700/50 ', {
+          'bg-amber-600/10 border-amber-600/20': isInFinalTimer,
+        })}
+        icon={Clock}
+        iconClassName={cn('text-rose-400', {
+          'text-amber-600': isInFinalTimer,
+        })}
+        label={isInFinalTimer ? 'Final Timer' : 'Normal Timer'}
       >
-        <Clock
-          className={cn('w-5 h-5 text-rose-400', {
-            'text-amber-600': isInFinalTimer,
-          })}
-        />
-        <div className=' text-zinc-300 flex flex-col leading-none lg:leading-tight lg:gap-1'>
-          <span className='text-[10px] text-zinc-400'>
-            {isInFinalTimer ? 'Final Timer' : 'Normal Timer'}
-          </span>
-          <PuzzleRemainingTime agent={agent} />
-        </div>
-      </div>
+        <PuzzleRemainingTime agent={agent} />
+      </Card>
 
-      <div className='flex items-center space-x-2 bg-rose-500/10 px-4 h-full rounded-xl border border-rose-500/20'>
-        <Coins className='w-5 h-5 text-rose-400' />
-        <span className=' font-bold text-white'>
-          <FormatCurrency
-            amount={agent.initial_prize + agent.total_fees_paid}
-            mint={mint}
-          />
-        </span>
+      <div className='flex gap-2 items-center h-full'>
+        <FeeCard fee={agent.current_fee} mint={mint} label='Current Fee' />
+        <FeeCard
+          fee={agent.initial_prize + agent.total_fees_paid}
+          mint={mint}
+          label='Pool'
+        />
       </div>
     </>
+  );
+}
+
+function FeeCard({
+  fee,
+  mint,
+  label,
+}: {
+  fee: number;
+  mint: anchor.web3.PublicKey;
+  label: string;
+}) {
+  return (
+    <Card
+      icon={Coins}
+      label={label}
+      className='bg-rose-500/10 border-rose-500/20'
+    >
+      <FormatCurrency amount={fee} mint={mint} />
+    </Card>
+  );
+}
+
+function Card({
+  label,
+  children,
+  icon: Icon,
+  className,
+  iconClassName,
+}: PropsWithChildren<{
+  label: string;
+  icon: LucideIcon;
+  className?: string;
+  iconClassName?: string;
+}>) {
+  return (
+    <div
+      className={cn(
+        'flex items-center space-x-2 px-4 h-full rounded-xl border leading-none',
+        className
+      )}
+    >
+      <Icon className={cn('w-5 h-5 text-rose-400', iconClassName)} />
+      <div className=' text-zinc-300 flex flex-col leading-none lg:leading-tight lg:gap-1'>
+        <span className='text-[10px] text-zinc-400'>{label}</span>
+        {children}
+      </div>
+    </div>
   );
 }
